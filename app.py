24c3e8d861eb3a3d3e6802abeb5831b3ca637b90
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-10-14 15:57:14 -0500

    work on rsvp form

--- app.py
+++ app.py
@@ -8,33 +8,50 @@ import json
 #init app and DB
 app = Flask(__name__)
 
-conn = sql.connect('static/wedding.db')
+app.config['RSVP-KEYS'] = ['ButlerWedding1', 'ButerWedding2']
 
 #############################################################
 #############################################################
 # Route to render index.html template
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
-@app.route("/rsvp", methods=["POST"])
+@app.route("/rsvp", methods=["GET", "POST"])
 def rsvp():
-    return render_template("index.html")
+    return render_template("rsvp.html")
 
-@app.route("/travel", methods=["POST"])
+@app.route("/travel", methods=["GET", "POST"])
 def travel():
-    return render_template("index.html")
+    return render_template("travel.html")
 
-@app.route("/schedule", methods=["POST"])
+@app.route("/schedule", methods=["GET", "POST"])
 def schedule():
-    return render_template("index.html")
+    return render_template("schedule.html")
 
-@app.route("/registry", methods=["POST"])
+@app.route("/registry", methods=["GET", "POST"])
 def registry():
-    return render_template("index.html")
-
+    return render_template("registry.html")
+@app.route("/rsvpConfirm", methods=["GET", "POST"])
+def rsvp_confirm():
+    if request.method == "POST":
+        rsvp_key = request.values["key"]
+        if rsvp_key in app.config['RSVP-KEYS']:
+            name = request.values["name"]
+            rsvp = request.values["rsvp"]
+            plus_one = request.values["plus-one"]
+            guest = (name, rsvp, plus_one)
+            conn = sql.connect('static/wedding.db')
+            cursor = conn.cursor()
+            insert = '''INSERT INTO guest_list(Name, RSVP, 'Plus One')
+            VALUES (?,?,?)'''
+            cursor.execute(insert, guest)
+            conn.close()
+            message = "RSVP confirmed. We can't wait to celebrate with you!"
+        else:
+            message = "RSVP failed. Please confirm your RSVP Key is correct and try again later."
+    return render_template("success.html", message=message)
 #############################################################
-conn.close()
 
 @app.after_request
 def add_header(r):
